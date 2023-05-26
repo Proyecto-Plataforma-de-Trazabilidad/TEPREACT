b6313ce5f48c391db943d517188f5bebff1739d7
SkalyBone <126298501+SkalyBone@users.noreply.github.com>
2023-05-25 20:25:56 -0600

    Add files via upload

---
+++ client/App.js
@@ -0,0 +1,21 @@
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import Home from './src/Home';
+import Drawer from './src/Drawer';
+
+const Stack = createStackNavigator();
+
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home" screenOptions={{headerShown:false}}>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Drawer" component={Drawer} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
